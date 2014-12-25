f250b18cf93d2d03eb1d4dae230cff63115ce7cf
Ron <RonDeBen@gmail.com>
2014-12-25 15:40:03 -0600

    I think music middleware is working, but I'll let Kinshy be the judge

--- Assets/Scripts/MusicMiddleware.cs
+++ Assets/Scripts/MusicMiddleware.cs
@@ -6,33 +6,61 @@ public class MusicMiddleware : MonoBehaviour {
 
     [System.Serializable]
     public class SoundEntry {
-        public string soundName;
         public AudioClip sound;
+        [HideInInspector]
         public AudioSource source;
+        [HideInInspector]
+        public bool looping;
+        public float loopStartTime;
+        public float loopEndTime;
     }
     private SoundEntry playEntry;
     public List<SoundEntry> sounds; 
 
-    public void loopSound(string name){
-        SoundEntry playEntry = sounds.Find(item => item.soundName == name);
+    public void loopSound(string name, bool startAtBeginning){
+        SoundEntry playEntry = sounds.Find(item => item.sound.name == name);
+        if(!startAtBeginning)
+            playEntry.source.time = playEntry.loopStartTime;
         playEntry.source.Play();
     }
 
-    public void loopSoundAfterTime(string name, float time) {
-        SoundEntry playEntry = sounds.Find(item => item.soundName == name);
+    public void loopFromTime(string name, float startTime, bool startAtBeginning){
+        SoundEntry playEntry = sounds.Find(item => item.sound.name == name);
+        if(startAtBeginning)
+            playEntry.source.time = startTime;
+        playEntry.source.Play();
+        playEntry.loopStartTime = startTime;
+        playEntry.looping = true;
+    }
 
+    public void loopBetweenTimes(string name, float startTime, float endTime, bool startAtBeginning){
+        SoundEntry playEntry = sounds.Find(item => item.sound.name == name);
+        if(startAtBeginning)
+            playEntry.source.time = startTime;
+        playEntry.source.Play();
+        playEntry.loopStartTime = startTime;
+        playEntry.loopEndTime = endTime;
+        playEntry.looping = true;
     }
 
     void Start() {
         foreach(SoundEntry sound in sounds){
             sound.source = gameObject.AddComponent<AudioSource>() as AudioSource;
             sound.source.clip = sound.sound;
+            sound.loopEndTime = sound.source.clip.length;
         }
-        playEntry = sounds.Find(item => item.soundName == "Very_Hedgie");
-        playEntry.source.Play();
+        loopSound("Very_Hedgie", true);
+        //loopFromTime("Very_Hedgie", 9.419f, true);
     }
 
-    void Update(){
-        Debug.Log("Song Time: " + playEntry.source.time + "\nGameTime" + Time.time);
+    void FixedUpdate(){
+        foreach(SoundEntry sound in sounds){
+            if(sound.looping){
+                if(!sound.source.isPlaying || sound.source.time >= sound.loopEndTime - 0.01){
+                    sound.source.Play();
+                    sound.source.time = sound.loopStartTime;
+                }
+            }
+        }
     }
 }
