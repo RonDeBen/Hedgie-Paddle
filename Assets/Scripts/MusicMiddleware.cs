610aa5a2f954c89734691037a0aa14189254be0a
RonDeBen <RonDeBen@gmail.com>
2014-12-25 14:31:51 -0600

    started working on a music middleware thingy

---
+++ Assets/Scripts/MusicMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicMiddleware : MonoBehaviour {
+
+    [System.Serializable]
+    public class SoundEntry {
+        public string soundName;
+        public AudioClip sound;
+        public AudioSource source;
+    }
+    private SoundEntry playEntry;
+    public List<SoundEntry> sounds; 
+
+    public void loopSound(string name){
+        SoundEntry playEntry = sounds.Find(item => item.soundName == name);
+        playEntry.source.Play();
+    }
+
+    public void loopSoundAfterTime(string name, float time) {
+        SoundEntry playEntry = sounds.Find(item => item.soundName == name);
+
+    }
+
+    void Start() {
+        foreach(SoundEntry sound in sounds){
+            sound.source = gameObject.AddComponent<AudioSource>() as AudioSource;
+            sound.source.clip = sound.sound;
+        }
+        playEntry = sounds.Find(item => item.soundName == "Very_Hedgie");
+        playEntry.source.Play();
+    }
+
+    void Update(){
+        Debug.Log("Song Time: " + playEntry.source.time + "\nGameTime" + Time.time);
+    }
+}
---
+++ Assets/Scripts/SongMap.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SongMap : MonoBehaviour {
+
+    [System.Serializable]
+    public class SongEntry {
+        public string songName;
+        public AudioSource audio;
+    }
+	
+}
